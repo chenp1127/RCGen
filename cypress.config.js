932d256e3cbc8755ce7047af8f778091b754287e
Hong Zhen <zhen.hong@ncs.com.sg>
2023-10-05 10:06:43 +0800

    Add in baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'https://main.d27k9wqkcnjscm.amplifyapp.com/',
   },
 });
