2ed278a42ecbca618419fb2410256172b8332a8d
kaiweitan <kaiwei.tan@ncs.com.sg>
2023-10-13 11:18:07 +0800

    formatting of recipe in singleitem

--- src/data/AllProductsData.js
+++ src/data/AllProductsData.js
@@ -29,7 +29,10 @@ export const allProductsData = [
     ItemImg: Cheesepizza,
     ItemName: 'Cheese Pizza',
     ItemDescription: 'Thick crusted pizza with 2 types of cheese',
-    Recipe: '1: Move oven rack to lowest position. Heat oven to 450°. Grease 12-inch pizza pan. Stir baking mix and water until soft dough forms. 2: Pat dough in pizza pan, using fingers dipped in baking mix; pinch edge, forming 1/2-inch rim. Spread pizza sauce over crust. Sprinkle with cheese. 3: Bake 12 to 15 minutes or until crust is brown and cheese is bubbly.',
+    Recipe: "1: Move oven rack to lowest position. Heat oven to 450°. Grease 12-inch pizza pan. Stir baking mix and water until soft dough forms."
+          + "\n2: Pat dough in pizza pan, using fingers dipped in baking mix; pinch edge, forming 1/2-inch rim. Spread pizza sauce over crust. Sprinkle with cheese."
+          + "\n3: Bake 12 to 15 minutes or until crust is brown and cheese is bubbly.",
+    CookingTime: '25',
     Category: 'Western',
     attributes: [
       {
@@ -50,7 +53,10 @@ export const allProductsData = [
     ItemImg: Chickenalfredopasta,
     ItemName: 'Chicken Alfredo Pasta',
     ItemDescription: 'Cream based chicken linguine with herbs',
-    Recipe: "1: Melt the butter in a large pan over medium-high heat, then add the chicken breasts. Season with the salt, pepper, oregano, and basil. Cook 8-10 minutes, or until the chicken is fully cooked. Remove the chicken from the pan and set aside. \n2: Make the sauce: In the same pan over medium heat, melt the butter, then add the garlic. Cook until the garlic begins to soften. Stir in half of the flour at a time until incorporated. Gradually pour in the milk, stirring between additions, and cook until fully incorporated and the sauce begins to thicken. Season with the salt, pepper, oregano, and basil. Add the Parmesan cheese and stir until melted. \n3: In a large bowl, pour the sauce over cooked penne pasta, then add the chicken and toss well to combine. Add the parsley and Parmesan and toss again.",
+    Recipe: "1: Melt the butter in a large pan over medium-high heat, then add the chicken breasts. Season with the salt, pepper, oregano, and basil."
+          + "\n2: Cook the chicken for 8-10 minutes, or until it is fully cooked. Remove the chicken from the pan and set aside."
+          + "\n3: To make the sauce, melt the butter in the same pan over medium heat, then add the garlic. Cook until the garlic begins to soften. Stir in half of the flour at a time until incorporated. Gradually pour in the milk, stirring between additions, and cook until fully incorporated and the sauce begins to thicken. Season with the salt, pepper, oregano, and basil. Add the Parmesan cheese and stir until melted."
+          + "\n4: In a large bowl, pour the sauce over cooked penne pasta, then add the chicken and toss well to combine. Add the parsley and Parmesan and toss again.",
     CookingTime: '30',
     //ItemPrice: (2).toFixed(2),
     Category: 'Western',
@@ -73,7 +79,11 @@ export const allProductsData = [
     ItemName: 'Caesar Salad',
     ItemDescription:
       'Green salad served with croutons and parmesan',
-    ItemPrice: (9).toFixed(2),
+    //ItemPrice: (9).toFixed(2),
+    Recipe: "1. In a medium bowl, whisk together the garlic, anchovy paste, lemon juice, Dijon mustard and Worcestershire sauce."
+          + "\n2. Add the mayonnaise, Parmigiano-Reggiano, salt and pepper and whisk until well combined. Taste and adjust to your liking. The dressing will keep well in the fridge for about a week."
+          + "\n3. Mix in your favourite vegetables, hard boiled eggs and croutons. Serve cold.",
+    CookingTime: '15',
     Category: 'Western',
     attributes: [
       {
@@ -118,7 +128,13 @@ export const allProductsData = [
     ItemName: 'Sweet and Sour Pork',
     ItemDescription:
       'Fried pork cubes glazed with tangy tomato based sauce',
-    ItemPrice: (8).toFixed(2),
+    //ItemPrice: (8).toFixed(2),
+    Recipe: "1.  Mix Pork with Marinade. Preheat oven to 80°C/175°F and place rack on tray - to keep pork warm."
+          + "\n2.  Place all Sauce ingredients EXCEPT water in a large jug or small bowl. Mix until combined, then mix in water."
+          + "\n3.  Pour enough oil into a large saucepan or small pot so it's 2.5cm / 1 inch deep. Heat to 180°C/350°F. Coat pork with cornstarch, and fry in batches for 3 minutes until golden. Keep warm in oven."
+          + "\n4.  To stir fry, heat oil in a very large skillet over high heat. Add garlic, ginger and onion, stir for 1 1/2 minutes. Add capsicum and stir for 2 minutes. Add Sauce & pinapple, then let it come to a rapid simmer. Simmer for 2 minutes until it thickens."
+          + "\n5.  Add pork, then quickly toss to coat for 10 seconds. Pour onto serving plate, serve immediately!",
+    CookingTime: '45',
     Category: 'Local',
     attributes: [
       {
@@ -140,7 +156,13 @@ export const allProductsData = [
     ItemName: 'Char Kway Teow',
     ItemDescription:
       'Fried thick noodles local style',
-    ItemPrice: (8).toFixed(2),
+    //ItemPrice: (8).toFixed(2),
+    Recipe: "1.  Heat lard and 1 tbsp oil in a wok or very large heavy based skillet over high heat. Swirl around the wok."
+          + "\n2.  When it starts smoking, add prawns. Cook for 30 seconds. Add garlic, stir for 10 seconds."
+          + "\n3.  Add noodles, then using both hands on the handle, toss 4 times until coated with oil (or gently fold using a spatula + wooden spoon). Add bean sprouts and garlic chives, toss or gently fold a few times."
+          + "\n4.  Push everything to one side, add remaining 1 tbsp oil. Add egg and cook, moving it around until mostly set - about 1 minute. Use wooden spoon to chop it up roughly."
+          + "\n5.  Pour Sauce over noodles, then toss to disperse Sauce through the noodles. Pause between tosses to give the noodles a chance to caramelise on the edges. Serve immediately.",
+    CookingTime: '20',
     Category: 'Local',
     attributes: [
       {
