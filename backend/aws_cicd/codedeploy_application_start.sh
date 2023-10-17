94899e21d27e312559c1e903dff46aa5a61694e3
Chen <55453449+chenp1127@users.noreply.github.com>
2023-10-18 00:35:36 +0800

    codedeploy config modify

--- backend/aws_cicd/codedeploy_application_start.sh
+++ backend/aws_cicd/codedeploy_application_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Stop all servers and start the server
-pm2 stop all
-pm2 start /home/ubuntu/rcgenbackend/backend/api/index.mjs
+sudo pm2 stop all
+sudo pm2 start /home/ubuntu/rcgenbackend/backend/api/index.mjs
