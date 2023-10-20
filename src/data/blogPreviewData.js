c7be1165dd73f1d1213227356625eaa1018c306b
kaiweitan <kaiwei.tan@ncs.com.sg>
2023-10-20 17:39:44 +0800

    blog posts landing page

---
+++ src/data/blogPreviewData.js
@@ -0,0 +1,173 @@
+import salt from "../assets/images/blog/salt.jpg";
+import bonehealth from "../assets/images/blog/bonehealth.jpg";
+import onthego from "../assets/images/blog/onthego.jpg";
+
+const blogPreviewData = [
+    {
+        img: salt,
+        name: "Which Salts Have the Lowest Sodium",
+        intro:
+          "Kosher, iodized, sea salt, and pink Himalayan salt: we break down which types of salt have the lowest sodium.",
+        date: "April 2, 2023",
+        author: "Kira Wilson",
+        id: salt,
+        content: `Iodized Salt
+        You may know the term "iodized salt" or recognize its packaging. Table salt is often fortified with iodine, which is important for producing thyroid hormones, according to the American Thyroid Association. If you don't have enough iodine, you may become at risk for an enlarged thyroid.
+        
+        Iodized salt is a great source of iodine and dissolves the quickest in food, making it ideal for most of your cooking and baking needs.
+        
+        Bottom line: One teaspoon of iodized salt contains about 2,360 milligrams of sodium (just above the recommended daily serving), according to the USDA. Use it in recipes with exact measurements and in the pasta water.
+    
+        Iodized Salt
+        You may know the term "iodized salt" or recognize its packaging. Table salt is often fortified with iodine, which is important for producing thyroid hormones, according to the American Thyroid Association. If you don't have enough iodine, you may become at risk for an enlarged thyroid.
+    
+        Iodized salt is a great source of iodine and dissolves the quickest in food, making it ideal for most of your cooking and baking needs.
+    
+        Bottom line: One teaspoon of iodized salt contains about 2,360 milligrams of sodium (just above the recommended daily serving), according to the USDA. Use it in recipes with exact measurements and in the pasta water.
+    
+        Sea Salt
+        Sea salt comes from evaporated ocean water or saltwater lakes. Because sea salt is less processed than iodized salt, it 1) has less iodine but 2) retains small amounts of minerals like potassium.
+    
+        Sea salt has bigger granules than other types of salt, which can potentially mean more flavor for less sodium. But skip this briny salt in everyday cooking or baking since it doesn't dissolve easily, which can cause issues with the taste and texture of dishes. It's more fun to sprinkle it on top of your dishes.
+    
+        Bottom line: One teaspoon of sea salt has about 2,000 milligrams of sodium, according to the USDA. Try it as a flavorful garnish for soups, salads, and even chocolate chip cookies.
+        
+        Low-Sodium Salt
+        Low-sodium salt is growing in popularity, but what exactly is it? It's sodium chloride (you know, salt) that has had some of its sodium replaced with potassium chloride, a mineral that tastes salty but is bitter when heated.
+    
+        While more research is needed to understand the full effects of low-sodium salt, a 2021 study published in The New England Journal of Medicine showed the potential for low-sodium salts to decrease rates of stroke, major cardiovascular events, and death. This study used a salt substitute that contained 75% sodium chloride and 25% potassium chloride.
+    
+        Bottom line: Low-sodium salt may be a good alternative to table salt if you're trying to reduce your sodium intake. However, if you have kidney disease or take certain medications, the NIH recommends you speak to a health provider before using low-sodium salts with potassium chloride.
+    
+        Pink Himalayan Salt
+        Pink Himalayan salt is mined in Pakistan near the Himalayas. You've maybe seen it at the dinner table in salt grinders, on a nightstand in a bedside lamp, or in spas, where "salt rooms" advertise non-dietary health benefits like stress or allergy relief.
+    
+        Pink Himalayan salt has slightly less sodium than table salt. It also has trace amounts of mineral elements (that help make it pink) like magnesium, zinc, iron, calcium, and potassium, but not enough to have an impact on your health.
+    
+        Pink Himalayan salt does have less iodine, which your body needs. The NIH notes that if you live in mountainous areas like the Himalayas, you are at risk for iodine deficiency unless you consume iodized salt.
+    
+        Importantly, not enough research supports the most popular health claims like stress, allergy, or muscle-soreness relief.
+    
+        Bottom line: A teaspoon of pink Himalayan salt has about 1,680 milligrams of sodium, according to the USDA. It may be a great addition to your meal. But we still need more research to understand the non-dietary benefits of pink Himalayan salt.
+        `
+      },
+    
+      {
+        img: bonehealth,
+        name: "Nutrition for bones",
+        intro:
+          "Eating and drinking the right things can help support your bone health at every stage of your life.",
+        date: "March 18, 2023",
+        author: "George Denson",
+        id: bonehealth,
+        content: `Calcium and vitamin D are two nutrients well-known to be important for bones. But there are many other vitamins, minerals and nutrients that are vital to help your bones stay healthy and strong.
+    
+        Try not to become too concerned with getting all these in your diet. If you enjoy a healthy, balanced diet, you're probably getting everything you need.
+        
+        A healthy, balanced diet
+        This is all about eating meals that have foods from the four main food groups:
+        
+        fruit and vegetables
+        carbohydrates, like bread, potatoes, pasta and cereals
+        dairy and alternatives
+        proteins, like beans, eggs, fish and meat
+        For more information about these food groups and how much of each you need, take a look at The Eatwell Guide.
+    
+        Calcium
+        Calcium gives your bones the strength and hardness they need to cope with your everyday activities.
+    
+        Your body contains about one kilogram of calcium - 99% of this is in your bones. Most people can get enough calcium through healthy eating alone.
+    
+        Foods containing calcium
+        Whatever your dietary needs, there are plenty of foods you can eat to get calcium through your diet. Foods rich in calcium include:
+    
+        -dairy products, like milk and cheese
+        -green leafy vegetables
+        -almonds
+        -sesame seeds
+        -sardines
+        -dried fruit
+        -pulses
+        -tofu
+        -fortified foods and drink, like breakfast cereal and alternative milks.
+    
+        Vitamin D for bones
+        Vitamin D helps your body absorb and use calcium, which gives your bones their strength and hardness.
+    
+        There are three ways you can get vitamin D:
+    
+        -From sunlight
+        -From food
+        -From supplements
+    
+        Low vitamin D levels could increase your risk of osteoporosis and broken bones. And a severe shortage of vitamin D causes rickets and osteomalacia, which is soft, weak bones.
+    
+        Sunlight
+        When the sun’s rays fall on your body, they react with your skin to make vitamin D. In the UK, your skin can only get vitamin D from sunlight between March and September. During this time, it's recommended you expose your skin to direct sunlight for around 10 minutes, once or twice per day.
+    
+        Give your skin short periods in the sun, without sunscreen, while you're doing short outdoor tasks such as: hanging out the washing, pulling up weeds and walking to the shops.
+    
+        Food
+        There's a small amount of vitamin D in some foods, but it's difficult to get enough vitamin D from food alone. Foods containing vitamin D include:
+    
+        -oily fish, such as herring, salmon and mackerel
+        -eggs
+        -some pork products
+        -lamb's liver
+        -fortified bread
+        -fortified yoghurts
+        -specially processed mushrooms.
+        `
+      },
+      
+      {
+        img: onthego,
+        name: "How to Eat Healthy When You Have No Time",
+        intro:
+          "Tips to make intentional healthy decisions when purchasing food even on a tight schedule.",
+        date: "March 7, 2023",
+        author: "Alex Lee",
+        id: onthego,
+        content: `You start with the best intentions: You bookmark healthy recipes, pick up kale and quinoa at the grocery store and buy a pressure cooker. But then something happens. You get slammed with a huge work project, you find yourself taking care of sick kids (or parents), or you spend all day running errands. Whatever the reason, life often leaves us no time (or energy!) to prepare meals. So you swing by the drive-thru or phone for a pizza, which leaves you feeling bloated and discouraged about ever eating healthy again.
+    
+        You’re right to be concerned. “A diet that’s high in saturated fat, sodium and sugar can lead to obesity , elevated blood cholesterol levels and high blood pressure — all of which are risk factors for heart disease,” says Isatu Isuk, R.D., L.D.N., a dietitian at The Johns Hopkins Hospital. But there are ways you can whip up wholesome meals at home without investing tons of time planning meals or grocery shopping. You can also eat healthier when you’re on the go. Isuk recommends these strategies to help you stay on track even when you’re feeling frazzled.
+        
+        Healthy Restaurant Options
+        Many restaurants now offer lighter fare on their menus, often with calorie counts. Take a quick look at restaurant menus online to see which ones provide the best options before you head out. Then consider these tips when ordering:
+        
+        1. Eat a small salad or broth-based soup first. It’s a nutritious and filling start to your meal that will help you consume fewer calories overall.
+        2. Choose veggie-based entrees or those with baked, broiled or grilled fish or skinless chicken.
+        3. Look for meals that aren’t greasy or oily. For example, instead of ordering cheese-covered chicken enchiladas, get the grilled chicken or fish tacos. Choose food items without creamy sauces or gravies.
+        4. Ask to substitute veggies or salad for fattening sides such as fries, coleslaw or butter-laden mashed potatoes.
+        
+        Portions served at restaurants can be too big. See if smaller portions are available. Restaurants often offer “half” salads or sandwiches upon request. If not, ask for a doggie bag and be prepared to take home one-third to half of the meal.
+        If you crave dessert, opt for fresh fruit or sorbet.
+    
+        Better Fast-Food Choices
+        Eating fast food doesn’t mean you have to down a fatty cheeseburger and salty fries. These days, there are a variety of menu options where you can find healthier picks. Many fast-food restaurants also have their menus, with calorie counts, online.
+        
+        Smart picks for a quick and easy meal are places where you have more control over what goes into your order. At a sandwich shop, it’s easy to make healthy adjustments such as no mayo, extra veggies and whole wheat bread. (Plus, they also often have salads and soups.) Ethnic eateries that offer build-your-own bowls, vegetarian options or grilled meat and vegetable plates are good bets, too.
+        
+        But if you’re passing through a traditional drive-thru, keep these tips in mind:
+        
+        Avoid fried food (e.g., french fries, onion rings, chicken strips and nuggets).
+        A grilled skinless chicken breast sandwich is lower in saturated fat than a burger.
+        Order the smallest sizes — don’t “supersize” anything. Ordering the kid’s menu version of your favorite fast food can give you the taste you crave with far fewer calories.
+        Skip calorie-heavy sides and toppings.
+        Bypass sugary drinks in favor of water, unsweetened tea, or fat-free or low-fat milk.
+    
+        Convenient Home-Cooked Meal Kits
+        If the time it takes to gather recipes, plan what you’re going to eat for a week and grocery shop is what’s holding you back from eating healthy meals at home, there’s an easy solution: meal subscription boxes.
+        
+        There are more than 100 different companies that box up multiple meals’ worth of groceries and deliver them to your house each week. Sure, the cost is more than if you did the meal planning and shopping yourself, but many find the convenience worth it. When your meal box is delivered, you can just throw the premeasured ingredients together and follow the cooking instructions to get portion-controlled, tasty dinners on the table in minutes. You get to choose from a constantly rotating list of recipes. Many companies offer low-carb, low-calorie, gluten-free or vegetarian options as well. And you avoid buying ingredients that go unused and sit in the fridge.
+        
+        “Whether you’re dining out or eating in, it’s important to think about a balanced diet,” says Isuk. “That means making sure you’re getting a good mix of lean protein, vegetables, fruits, whole grains and healthy low-fat dairy.”
+    
+        Or you can just use RCGen. We provide you with a variety of recipes that are easy to understand even for amateur home chefs.
+        
+        With such a wide range of convenient meal options available — and the information you need to make smart, heart-healthy choices — you can feel good about what you eat even when life gets busy.
+        `
+      },
+]
+
+export default blogPreviewData;
