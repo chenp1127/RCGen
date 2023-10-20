bfc6fcc7399c83a39d02070d5130891693373282
Laurette Jek (NCS) <laurette.jek@ncs.com.sg>
2023-10-20 16:39:29 +0800

    Fixed Pricing at Checkout

--- src/routes/checkout/CheckoutItem.js
+++ src/routes/checkout/CheckoutItem.js
@@ -1,17 +1,36 @@
 import React, { useEffect, useState } from "react";
+import { allProductsData } from "../../data/AllProductsData";
 
 const CheckoutItem = ({ cartItem }) => {
-  const [selectedAttributes, setSelectedAttributes] = useState([]);
-  const totalPrice = cartItem.userSelectedAttributes.reduce(
-    (acc, attribute) => acc + parseFloat(attribute.price),
-    0
-  );
+  const [totalPrice, setTotalPrice] = useState(0);
 
   useEffect(() => {
-    cartItem.userSelectedAttributes.map((item) => {
-      return setSelectedAttributes(item.attributeValue);
-    });
-  }, [cartItem.userSelectedAttributes]);
+    // Calculate total price based on user-selected attributes and quantity
+    const calculateTotalPrice = () => {
+      const product = allProductsData.find((item) => item.id === cartItem.id);
+
+      // Calculate total price for the current cart item
+      const itemTotalPrice = cartItem.userSelectedAttributes.reduce(
+        (itemPrice, attribute) => {
+          const attributeGroup = product.attributes.find(
+            (attr) => attr.id === attribute.attributeId
+          );
+          const selectedOption = attributeGroup.attributeOptions.find(
+            (option) => option.id === attribute.attributeValue
+          );
+          const attributePrice = selectedOption ? selectedOption.price : 0;
+          return itemPrice + attributePrice;
+        },
+        0
+      );
+
+      // Update the state with the calculated total price
+      setTotalPrice(itemTotalPrice * cartItem.quantity);
+    };
+
+    // Call the function to calculate total price whenever cartItem or its attributes change
+    calculateTotalPrice();
+  }, [cartItem]);
 
   return (
     <section className="checkout-item">
@@ -21,7 +40,12 @@ const CheckoutItem = ({ cartItem }) => {
           <h3>{cartItem.ItemName}</h3>
         ) : (
           <h3>
-            {cartItem.ItemName} <span>{selectedAttributes}</span>
+            {cartItem.ItemName}{" "}
+            <span>
+              {cartItem.userSelectedAttributes
+                .map((attribute) => attribute.attributeValue)
+                .join(", ")}
+            </span>
           </h3>
         )}
         <p>Quantity: {cartItem.quantity}</p>
